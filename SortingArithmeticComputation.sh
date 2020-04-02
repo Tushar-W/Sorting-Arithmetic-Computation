734ddb7c0f89b4903b1f581f4a33e7590ce35b2e
Tushar-W <twankhede95@gmail.com>
2020-04-02 16:45:07 +0530

    ADDED:function for multiply and add three numbers

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -x
 
 echo "Sorting Arithmetic Computation"
-
 function addAndMultiplyNum() {
-	a=$1	b=$2 c=$3
+	a=$1 b=$2 c=$3
 	echo result=$(($a + $b * $c))
 }
 function multiplyAndAddNum() {
-	a=$1	b=$2 c=$3
+	a=$1 b=$2 c=$3
 	echo result=$(($a * $b + $c))
 }
-
 read -p "Enters Three Numbers:" a b c
 echo result=$( addAndMultiplyNum $a $b $c )
 echo result=$( multiplyAndAddNum $a $b $c )
