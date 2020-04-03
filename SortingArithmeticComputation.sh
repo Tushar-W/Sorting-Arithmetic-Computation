b40b283f2794b60a5083a592a499da89bc9ad25a
Tushar-W <twankhede95@gmail.com>
2020-04-03 20:08:11 +0530

    REFACTORED:functions for storing values in dictionary and into array

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -8,22 +8,27 @@ function addAndMultiplyNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a + $b * $c))
 }
+
 function multiplyAndAddNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a * $b + $c))
 }
+
 function addAndDivideNum() {
 	a=$1 b=$2 c=$3
 	echo $(($c + $a / $b))
 }
+
 function modAndAddNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a % $b + $c))
 }
+
 function sortDescendingOrder() {
-	for (( count=0; count<4; ++count ))
+
+	for (( count=0; count<${#resultsInArray[@]}; ++count ))
 	do
-		for (( j=$((count+1)); j<4; ++j))
+		for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
 		do
 			if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
 			then
@@ -33,11 +38,14 @@ function sortDescendingOrder() {
 			fi
 		done
 	done
+
 }
+
 function sortAscendingOrder() {
-	for (( count=0; count<4; ++count ))
+
+	for (( count=0; count<${#resultsInArray[@]}; ++count ))
 	do
-		for (( j=$((count+1)); j<4; ++j))
+		for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
 		do
 			if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
 			then
@@ -47,17 +55,32 @@ function sortAscendingOrder() {
 			fi
 		done
 	done
+
 }
+
+function storeResultsInDictionary() {
+
+	computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
+	computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
+	computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
+	computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
+
+}
+
+function storeValuesInArray() {
+
+	resultsInArray[0]=${computeResults["addAndMultiply"]}
+	resultsInArray[1]=${computeResults["multiplyAndAdd"]}
+	resultsInArray[2]=${computeResults["addAndDivide"]}
+	resultsInArray[3]=${computeResults["modAndAdd"]}
+
+}
+
 read -p "Enters Three Numbers:" a b c
-computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
-computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
-computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
-computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
-#reading values from dictionary into Array
-resultsInArray[0]=${computeResults["addAndMultiply"]}
-resultsInArray[1]=${computeResults["multiplyAndAdd"]}
-resultsInArray[2]=${computeResults["addAndDivide"]}
-resultsInArray[3]=${computeResults["modAndAdd"]}
+#store all results in dictionary
+storeResultsInDictioanry
+#store values in Array from dictionary
+storeValuesInArray
 # sorting values of array in Descending order
 sortDescendingOrder
 echo ${resultsInArray[@]}
@@ -66,4 +89,3 @@ echo ${!resultsInArray[@]}
 sortAscendingOrder
 echo ${resultsInArray[@]}
 echo ${!resultsInArray[@]}
-
