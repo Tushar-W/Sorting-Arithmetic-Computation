be3e9e32bee30fc3e4133f9c4a5e0f8bad4cfc2e
Tushar-W <twankhede95@gmail.com>
2020-04-03 20:26:22 +0530

    REFACTORED:function for storing values in array from dictionary

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -8,27 +8,46 @@ function addAndMultiplyNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a + $b * $c))
 }
+
 function multiplyAndAddNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a * $b + $c))
 }
+
 function addAndDivideNum() {
 	a=$1 b=$2 c=$3
 	echo $(($c + $a / $b))
 }
+
 function modAndAddNum() {
 	a=$1 b=$2 c=$3
 	echo $(($a % $b + $c))
 }
+
+function storeResultsInDictionary() {
+
+	computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
+	computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
+	computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
+	computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
+
+}
+
+function storeValuesInArray() {
+
+	resultsInArray[0]=${computeResults["addAndMultiply"]}
+	resultsInArray[1]=${computeResults["multiplyAndAdd"]}
+	resultsInArray[2]=${computeResults["addAndDivide"]}
+	resultsInArray[3]=${computeResults["modAndAdd"]}
+
+}
+
 read -p "Enters Three Numbers:" a b c
-computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
-computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
-computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
-computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
-#reading values from dictionary into Array
-resultsInArray[0]=${computeResults["addAndMultiply"]}
-resultsInArray[1]=${computeResults["multiplyAndAdd"]}
-resultsInArray[2]=${computeResults["addAndDivide"]}
-resultsInArray[3]=${computeResults["modAndAdd"]}
+#store all results in dictionary
+storeResultsInDictionary
+echo ${computeResults[@]}
+echo ${!computeResults[@]}
+#store values in Array from dictionary
+storeValuesInArray
 echo ${resultsInArray[@]}
 echo ${!resultsInArray[@]}
