a8b0a21d6dd9e5124c404354a93f438040765170
Tushar-W <twankhede95@gmail.com>
2020-04-04 14:36:37 +0530

    REFACTORED:merge sorting function descending&ascending order

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -5,32 +5,28 @@ declare -A computeResults
 declare -a resultsInArray
 
 function addAndMultiplyNum() {
-	a=$1 b=$2 c=$3
 	echo $(($a + $b * $c))
 }
 
 function multiplyAndAddNum() {
-	a=$1 b=$2 c=$3
 	echo $(($a * $b + $c))
 }
 
 function addAndDivideNum() {
-	a=$1 b=$2 c=$3
 	echo $(($c + $a / $b))
 }
 
 function modAndAddNum() {
-	a=$1 b=$2 c=$3
 	echo $(($a % $b + $c))
 }
 
-function sortDescendingOrder() {
-
+function sortArray() {
+	operand=$1
 	for (( count=0; count<${#resultsInArray[@]}; ++count ))
 	do
 		for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
 		do
-			if [ ${resultsInArray[count]} -lt ${resultsInArray[j]} ];
+			if [ ${resultsInArray[count]} $operand ${resultsInArray[j]} ];
 			then
 				temp=${resultsInArray[count]}
 				resultsInArray[count]=${resultsInArray[j]}
@@ -38,42 +34,20 @@ function sortDescendingOrder() {
 			fi
 		done
 	done
-
-}
-
-function sortAscendingOrder() {
-
-	for (( count=0; count<${#resultsInArray[@]}; ++count ))
-	do
-		for (( j=$((count+1)); j<${#resultsInArray[@]}; ++j))
-		do
-			if [ ${resultsInArray[count]} -gt ${resultsInArray[j]} ];
-			then
-				temp=${resultsInArray[count]}
-				resultsInArray[count]=${resultsInArray[j]}
-				resultsInArray[j]=$temp
-			fi
-		done
-	done
-
 }
 
 function storeResultsInDictionary() {
-
-	computeResults["addAndMultiply"]=$( addAndMultiplyNum $a $b $c )
-	computeResults["multiplyAndAdd"]=$( multiplyAndAddNum $a $b $c )
-	computeResults["addAndDivide"]=$( addAndDivideNum $a $b $c )
-	computeResults["modAndAdd"]=$( modAndAddNum $a $b $c )
-
+	computeResults["addAndMultiply"]=$( addAndMultiplyNum )
+	computeResults["multiplyAndAdd"]=$( multiplyAndAddNum )
+	computeResults["addAndDivide"]=$( addAndDivideNum )
+	computeResults["modAndAdd"]=$( modAndAddNum )
 }
 
 function storeValuesInArray() {
-
 	resultsInArray[0]=${computeResults["addAndMultiply"]}
 	resultsInArray[1]=${computeResults["multiplyAndAdd"]}
 	resultsInArray[2]=${computeResults["addAndDivide"]}
 	resultsInArray[3]=${computeResults["modAndAdd"]}
-
 }
 
 read -p "Enters Three Numbers:" a b c
@@ -86,9 +60,8 @@ storeValuesInArray
 echo ${resultsInArray[@]}
 echo ${!resultsInArray[@]}
 #shows all computation in descending order by sorting array
-sortDescendingOrder
+sortArray -lt
 echo ${resultsInArray[@]}
 #shows all computation in ascending order by sorting array
-sortAscendingOrder
+sortArray -gt
 echo ${resultsInArray[@]}
-
